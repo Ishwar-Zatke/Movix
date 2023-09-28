10a241be96f6844cc524647c3ed58164db418980
Ishwarzatke672 <97915434+Ishwar-Zatke@users.noreply.github.com>
2023-09-28 22:59:53 +0530

    Footer Updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,35 +14,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
+                
                 <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    Movix is your go-to platform for discovering the latest trends in movies and shows. Our mission is to provide a seamless and intuitive media exploration experience. With a vast array of categories and detailed insights into each title, we aim to cater to every movie enthusiast's curiosity. Enjoy endless scrolling through our carefully curated lists, and find your next favorite show or movie right here on Movix!
                 </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
+                
             </ContentWrapper>
         </footer>
     );
